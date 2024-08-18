68e1cf6fd060fc1e5eed249f7d9b3eae28dc8b2e
Travier Moorlag <moorlagt@gmail.com>
2024-08-18 11:43:00 -0400

    more work on b+ tree

--- bplustree_test.go
+++ bplustree_test.go
@@ -126,20 +126,65 @@ func TestIsShapedCorrectlyAfter6(t *testing.T) {
 	tree.Print()
 }
 
-func TestCanFindLeaf(t *testing.T) {
-	n1 := &BPlusTreeNode{keys: []int{1, 2, 3}, isLeaf: true}
-	n2 := &BPlusTreeNode{keys: []int{4, 5, 6}, isLeaf: true}
-	n3 := &BPlusTreeNode{keys: []int{7, 8, 9}, isLeaf: true}
-	n4 := &BPlusTreeNode{keys: []int{10, 11, 12}, isLeaf: true}
+// func TestIsShapedCorrectlyAfter7(t *testing.T) {
+// 	tree := NewBPlusTree(3)
+// 	keys := []int{1, 2, 3, 4, 5, 6, 7}
 
-	root := &BPlusTreeNode{
-		keys:     []int{4, 7},
-		children: []*BPlusTreeNode{n1, n2, n3},
-		isLeaf:   false,
-	}
+// 	for _, key := range keys {
+// 		tree.Insert(key, fmt.Sprintf("Node #%d", key))
+// 	}
 
-	root.next = n4 // example of linking nodes sequentially
-	tree := NewBPlusTree(3)
+// 	tree.Print()
+// 	return
 
-	tree.Print()
-}
+// 	// root
+// 	assert.Equal(t, []int{3, 5}, tree.root.keys)
+
+// 	// 2nd layer
+// 	assert.Equal(t, []int{2}, tree.root.children[0].keys) // right
+// 	assert.Equal(t, []int{4}, tree.root.children[1].keys) // middle
+// 	assert.Equal(t, []int{6}, tree.root.children[2].keys) // left
+
+// 	// 3rd layer
+// 	// left
+// 	assert.Equal(t, []int{1}, tree.root.children[0].children[0].keys)
+// 	assert.Equal(t, []int{2}, tree.root.children[0].children[1].keys)
+
+// 	// middle
+// 	assert.Equal(t, []int{3}, tree.root.children[1].children[0].keys)
+// 	assert.Equal(t, []int{4}, tree.root.children[1].children[1].keys)
+
+// 	// right
+// 	assert.Equal(t, []int{5}, tree.root.children[2].children[0].keys)
+// 	assert.Equal(t, []int{6, 7}, tree.root.children[2].children[1].keys)
+
+// 	tree.Print()
+// }
+
+// func TestCanHandleOneHundredNodes(t *testing.T) {
+// 	tree := NewBPlusTree(3)
+
+// 	for i := 0; i < 8; i++ {
+// 		tree.Insert(i, fmt.Sprintf("Node #%d", i))
+// 	}
+
+// 	tree.Print()
+// }
+
+// func TestCanFindLeaf(t *testing.T) {
+// 	n1 := &BPlusTreeNode{keys: []int{1, 2, 3}, isLeaf: true}
+// 	n2 := &BPlusTreeNode{keys: []int{4, 5, 6}, isLeaf: true}
+// 	n3 := &BPlusTreeNode{keys: []int{7, 8, 9}, isLeaf: true}
+// 	n4 := &BPlusTreeNode{keys: []int{10, 11, 12}, isLeaf: true}
+
+// 	root := &BPlusTreeNode{
+// 		keys:     []int{4, 7},
+// 		children: []*BPlusTreeNode{n1, n2, n3},
+// 		isLeaf:   false,
+// 	}
+
+// 	root.next = n4 // example of linking nodes sequentially
+// 	tree := NewBPlusTree(3)
+
+// 	tree.Print()
+// }
